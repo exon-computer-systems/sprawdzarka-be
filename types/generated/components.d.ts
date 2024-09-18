64cfb59021aa493a60d11d7369506a00c9b1ea67
Szymon <szmn.rtn@gmail.com>
2024-09-18 11:40:44 +0200

    Init git

---
+++ types/generated/components.d.ts
@@ -0,0 +1,39 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface PostPost extends Schema.Component {
+  collectionName: 'components_post_posts';
+  info: {
+    displayName: 'Post';
+    icon: 'dashboard';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    media: Attribute.Media<'images' | 'videos'> & Attribute.Required;
+    duration: Attribute.Integer;
+    brand: Attribute.Relation<'post.post', 'oneToOne', 'api::brand.brand'>;
+    playsCount: Attribute.Integer & Attribute.DefaultTo<0>;
+    playsTime: Attribute.Integer & Attribute.DefaultTo<0>;
+    fullPlaysCount: Attribute.Integer & Attribute.DefaultTo<0>;
+  };
+}
+
+export interface PostPlaylist extends Schema.Component {
+  collectionName: 'components_post_playlists';
+  info: {
+    displayName: 'Playlist';
+    icon: 'check';
+  };
+  attributes: {
+    post: Attribute.Component<'post.post', true>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'post.post': PostPost;
+      'post.playlist': PostPlaylist;
+    }
+  }
+}
